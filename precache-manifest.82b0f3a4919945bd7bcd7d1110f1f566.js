ffc52d950a064fbf7eede70ad19ff70775aa514a
Alejandro Gemas <alejandro.gemas@gmail.com>
2020-10-11 18:21:39 +0200

    Updates

---
+++ precache-manifest.82b0f3a4919945bd7bcd7d1110f1f566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d960340c0af6aa48789af020c2c756e",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "fe63b6376122f6a0cd39",
+    "url": "/note-app/static/css/main.7d34bf7b.chunk.css"
+  },
+  {
+    "revision": "e237d2c4a80367890fd4",
+    "url": "/note-app/static/js/2.87d201e8.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/note-app/static/js/2.87d201e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe63b6376122f6a0cd39",
+    "url": "/note-app/static/js/main.b90008b7.chunk.js"
+  },
+  {
+    "revision": "52c0b3008706d1f9b16c",
+    "url": "/note-app/static/js/runtime-main.91f093c2.js"
+  }
+]);
